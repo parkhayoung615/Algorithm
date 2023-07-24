8a11ee02193ebb848023db5002ccb5185c9ed445
parkhayoung615 <parkhayoung615@gmail.com>
2023-07-24 12:40:04 +0900

    backup

---
+++ b2751.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(int(sys.stdin.readline()))
+data.sort()
+
+for i in data:
+    print(i)
---
+++ b2754.py
@@ -0,0 +1,21 @@
+def switch(key):
+    telecom = {
+        "A+": "4.3",
+        "A0": "4.0",
+        "A-": "3.7",
+        "B+": "3.3",
+        "B0": "3.0",
+        "B-": "2.7",
+        "C+": "2.3",
+        "C0": "2.0",
+        "C-": "1.7",
+        "D+": "1.3",
+        "D0": "1.0",
+        "D-": "0.7",
+        "F": "0.0"
+    }.get(key, "알 수 없는")
+    print(f'{telecom}')
+
+
+firstNumber = input()
+switch(firstNumber)
