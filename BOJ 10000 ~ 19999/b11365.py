3abd3649abe27137f6286ffa00535feb57afa69b
parkhayoung615 <parkhayoung615@gmail.com>
2023-07-26 15:16:17 +0900

    Update b11365.py

--- BOJ 10000 ~ 19999/b11365.py
+++ BOJ 10000 ~ 19999/b11365.py
@@ -1,5 +1,16 @@
+# !밀비 급일
+
 while(1):
     c = (input())
     if(c == 'END'):
         break
     print(c[::-1])
+
+
+# # 이렇게 했을 땐 안 됐음 눈물만 난다 ,,
+# while(1):
+#     c = list(input())
+#     if(c == 'END'):
+#         break
+#     for i in reversed(c):
+#         print(i, end='')
